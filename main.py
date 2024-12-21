8ae16ab7c853e63c90a68833a3348b0b517697ac
Ilsaf <sharibzhanov.ilsaf@mail.ru>
2024-12-21 20:27:27 +0300

    Example of creating task

--- main.py
+++ main.py
@@ -15,17 +15,66 @@ def main():
 
     login = "some@example.com"
     password = "topsecret"
-    id_mpei_company = "id_mpei_company"
 
     yougile = YouGileRestAPI()
 
+    # Авторизация и получение списка компаний
+    companies = yougile.get_companies(login, password)
+    print(f"List of companies: {companies}")
+
+    id_mpei_company = companies[0].get('id') # ID компании
+    #id_test_company = companies[1].get('id')
     keys = yougile.get_keys(login, password, id_mpei_company)
-    print(keys)
-    projects = yougile.get_projects(keys[0].get('key'))
-    print(projects)
-    print(yougile.get_boards(keys[0].get('key')))
-    print(yougile.get_columns(keys[0].get('key')))
-    print(yougile.get_tasks(keys[0].get('key')))
+    print(f"List of keys: {keys}")
+    token = keys[0].get('key')  # Берём первый ключ авторизации
+    print(f"Authorization token: {token}")
+
+    users = yougile.get_users(token)
+    adminId = users[0].get('id')
+
+    # Создание нового проекта
+    project_title = "MPEI Schedule Project"
+    users = [{adminId: "admin"}]  # Можно добавить пользователей, если необходимо
+    new_project = yougile.create_project(token, project_title, users[0])
+    project_id = new_project.get('id')
+    print(f"Created project: {new_project}")
+
+    # Создание доски в проекте
+    board_title = "Schedule Board"
+    stickers = {"deadline": True, "stopwatch": True, "assignee": True, "custom": {}}
+    new_board = yougile.create_board(token, board_title, project_id, stickers)
+    board_id = new_board.get('id')
+    print(f"Created board: {new_board}")
+
+    # Создание колонки на доске
+    column_title = "Upcoming Tasks"
+    column_color = 10  # #EB3737 - Красный цвет
+    new_column = yougile.create_column(token, column_title, column_color, board_id)
+    column_id = new_column.get('id')
+    print(f"Created column: {new_column}")
+
+    # Создание задачи в колонке
+    task_title = "First Task"
+    subtasks = []  # Список подзадач
+    assigned = []  # ID пользователей, которым назначена задача
+    deadline = None  # Дедлайн задачи
+    time_tracking = None  # Тайм-трекинг
+    checklists = []  # Чек-листы
+    stickers = {}  # Стикеры
+
+    new_task = yougile.create_task(
+        token,
+        task_title,
+        column_id,
+        subtasks,
+        assigned,
+        deadline,
+        time_tracking,
+        checklists,
+        stickers,
+        description="Task created automatically for the project.",
+    )
+    print(f"Created task: {new_task}")
 
 if __name__ == "__main__":
     main()
