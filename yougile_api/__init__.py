3fdef4087437869e68486fb0578b4f7efc37ad0a
Ilsaf <sharibzhanov.ilsaf@mail.ru>
2025-05-25 21:07:53 +0300

    Updated yougile_api: updated __init__.py

--- yougile_api/__init__.py
+++ yougile_api/__init__.py
@@ -1,7 +1,14 @@
 """
-Модуль для работы с API YouGile
+------------------------------------------------------------
+YouGile API Client
+------------------------------------------------------------
+Библиотека для работы с REST API YouGile v2.0
+Документация API: https://ru.yougile.com/api-v2#/
+------------------------------------------------------------
 """
 
 from .YouGileRestAPI import YouGileRestAPI
+from .exceptions import YouGileAPIError
 
 __all__ = ['YouGileRestAPI']
+__version__ = "1.0.1"
